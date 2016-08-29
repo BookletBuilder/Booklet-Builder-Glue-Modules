99cf6881bce164d12e6cd491feea6e3dda125d5e
Biagio Arobba <biagio@ch-labs.com>
2016-08-29 18:47:07 +0000

    Added back legacy replacement pattern

--- olclangreplace/olclangreplace_data.example.php
+++ olclangreplace/olclangreplace_data.example.php
@@ -2,6 +2,9 @@
 
 $olclangreplace_data = array(
 
+    '{Language being studied}' => 'Language being studied',
+    '{Language for communicating instructions}' => 'Language for giving lesson',
+
     '{{LanguageBeingStudied}}' => 'Language being studied',
     '{{LanguageForCommunicatingInstructions}}' => 'Language for giving lesson'
 
