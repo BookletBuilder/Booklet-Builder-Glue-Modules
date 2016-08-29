f1b87a5395dbc523db613804d4730d47f4ad23ef
Biagio Arobba <biagio@ch-labs.com>
2016-08-29 17:38:15 +0000

    Added example data file

---
+++ olclangreplace/olclangreplace_data.example.php
@@ -0,0 +1,9 @@
+<?php
+
+$olclangreplace_data = array(
+
+    '{{LanguageBeingStudied}}' => 'Language being studied',
+    '{{LanguageForCommunicatingInstructions}}' => 'Language for giving lesson'
+
+);
+
